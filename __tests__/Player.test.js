dce1cafc2d3c9f82fe83be2577c09b640c4c999f
xunvyre <victoria@thedavids.us>
2022-08-13 11:39:59 -0400

    player object updated

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -31,3 +31,29 @@ test('gets inventory from player or returns false', () =>
     player.inventory = [];
     expect(player.getInventory()).toEqual(false);
 });
+
+test('gets player health value', () =>
+{
+    const player = new Player('Meatloaf');
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive for not', () =>
+{
+    const player = new Player('Meatloaf');
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test('subtracts from player health', () =>
+{
+    const player = new Player('Meatloaf');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth - 5);
+    player.reduceHealth(9999);
+    expect(player.health).toBe(0);
+});
